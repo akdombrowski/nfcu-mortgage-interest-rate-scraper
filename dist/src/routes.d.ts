48b73a6e4d49042eab6eda141a053f81387241b7
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-06-25 15:58:59 -0500

    build with tsc

--- dist/src/routes.d.ts
+++
@@ -1,4 +0,0 @@
-import { Dictionary } from "crawlee";
-export declare const router: import("crawlee").RouterHandler<import("crawlee").PuppeteerCrawlingContext<Dictionary>>;
-export default router;
-//# sourceMappingURL=routes.d.ts.map
