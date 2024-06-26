39d1659b061e073e9f9f99defb9416996a8f9845
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-06-26 08:14:35 -0500

    new ts build

--- dist/src/main.d.ts
+++ dist/src/main.d.ts
@@ -1,2 +1,4 @@
-export {};
+export interface ScrapedLoanData {
+    [key: string]: any;
+}
 //# sourceMappingURL=main.d.ts.map
--- dist/src/main.js
+++ dist/src/main.js
@@ -1,82 +1,95 @@
 import { Actor } from "apify";
 import { PuppeteerCrawler } from "crawlee";
-(async () => {
-    await Actor.main(async () => {
-        const startUrls = [
-            "https://www.navyfederal.org/loans-cards/mortgage/mortgage-rates/conventional-fixed-rate-mortgages.html",
-        ];
-        // Create a PuppeteerCrawler that will use the proxy configuration and and handle requests with the router from routes.js file.
-        const crawler = new PuppeteerCrawler({
-            // proxyConfiguration,
-            // requestHandler: router,
-            launchContext: {
-                launchOptions: {
-                    headless: true,
-                },
+await Actor.main(async () => {
+    const startUrls = [
+        "https://www.navyfederal.org/loans-cards/mortgage/mortgage-rates/conventional-fixed-rate-mortgages.html",
+    ];
+    // Create a PuppeteerCrawler that will use the proxy configuration and and handle requests with the router from routes.js file.
+    const crawler = new PuppeteerCrawler({
+        // proxyConfiguration,
+        // requestHandler: router,
+        launchContext: {
+            launchOptions: {
+                headless: true,
             },
-            maxRequestRetries: Number(process.env.MAX_REQUEST_RETRIES) ?? 0,
-            maxRequestsPerCrawl: Number(process.env.MAX_REQUESTS_PER_CRAWL) ?? 1,
-            navigationTimeoutSecs: Number(process.env.NAVIGATION_TIMEOUT_SECS) ?? 90,
-            requestHandlerTimeoutSecs: Number(process.env.REQUEST_HANDLER_TIMEOUT_SECS) ?? 120,
-            // Activates the Session pool (default is true).
-            useSessionPool: process.env.USE_SESSION_POOL?.toLowerCase() === "false" ? false : true,
-            // Overrides default Session pool configuration
-            sessionPoolOptions: {
-                maxPoolSize: Number(process.env.MAX_POOL_SIZE) ?? 100,
-            },
-            // Set to true if you want the crawler to save cookies per session,
-            // and set the cookies to page before navigation automatically (default is true).
-            persistCookiesPerSession: process.env.PERSIST_COOKIES_PER_SESSION?.toLowerCase() === "false"
-                ? false
-                : true,
-            autoscaledPoolOptions: { maxConcurrency: 1 },
-            async requestHandler(ctx) {
-                const { request, page, log, session, pushData } = ctx;
-                const title = await page.title();
-                const url = request.loadedUrl;
-                log.info("crawling", { title, url });
-                if (session) {
-                    if (title === "Blocked") {
-                        session.retire();
-                    }
-                    else if (title === "Not sure if blocked, might also be a connection error") {
-                        session.markBad();
-                    }
-                    // this step is done automatically in PuppeteerCrawler.
-                    // else { session.markGood() }
+        },
+        maxRequestRetries: Number(process.env.MAX_REQUEST_RETRIES) ?? 0,
+        maxRequestsPerCrawl: Number(process.env.MAX_REQUESTS_PER_CRAWL) ?? 2,
+        navigationTimeoutSecs: Number(process.env.NAVIGATION_TIMEOUT_SECS) ?? 60,
+        requestHandlerTimeoutSecs: Number(process.env.REQUEST_HANDLER_TIMEOUT_SECS) ?? 60,
+        // Activates the Session pool (default is true).
+        useSessionPool: process.env.USE_SESSION_POOL?.toLowerCase() === "false" ? false : true,
+        // Overrides default Session pool configuration
+        sessionPoolOptions: {
+            maxPoolSize: Number(process.env.MAX_POOL_SIZE) ?? 100,
+        },
+        // Set to true if you want the crawler to save cookies per session,
+        // and set the cookies to page before navigation automatically (default is true).
+        persistCookiesPerSession: process.env.PERSIST_COOKIES_PER_SESSION?.toLowerCase() === "false"
+            ? false
+            : true,
+        autoscaledPoolOptions: {
+            maxConcurrency: Number(process.env.AUTOSCALE_POOL_MAX_CONCURRENCY) ?? 1,
+        },
+        async requestHandler(ctx) {
+            const { request, page, log, session, pushData } = ctx;
+            const title = await page.title();
+            const url = request.loadedUrl;
+            log.info("crawling", { title, url });
+            if (session) {
+                if (title === "Blocked") {
+                    session.retire();
                 }
-                const tableSelector = "table tbody";
-                await page.waitForSelector(tableSelector);
-                const interestRatesTable = await page.$(tableSelector);
-                if (interestRatesTable) {
-                    const tableData = await interestRatesTable.$$eval("tr", async (els) => {
-                        const data = {};
-                        for (const tr of els) {
-                            const children = tr.children;
-                            const rowData = [];
-                            const loanType = children.item(0)?.textContent;
-                            if (loanType) {
-                                data[loanType] = rowData;
-                                for (const c of children) {
-                                    const content = c.textContent;
-                                    if (content) {
-                                        rowData.push(c.textContent);
-                                    }
+                else if (title === "Not sure if blocked, might also be a connection error") {
+                    session.markBad();
+                }
+                // this step is done automatically in PuppeteerCrawler.
+                // else { session.markGood() }
+            }
+            const tableSelector = ".rates-table table";
+            await page.waitForSelector(tableSelector);
+            // const interestRatesTable = await page.$(tableSelector);
+            try {
+                const tableData = await page.$eval(tableSelector, async (table) => {
+                    // const data: { [key: string]: any } = {};
+                    const data = [];
+                    const tHeader = table.tHead?.innerText;
+                    // Should look something like this:
+                    //  ['Term', 'Interest Rates As Low As', 'Discount Points', 'APR As Low As']
+                    const cols = tHeader?.replace("\n", "").split("\t");
+                    const tBodies = table.tBodies;
+                    if (tBodies && tBodies.length > 0) {
+                        const tBody = tBodies[0];
+                        const tRows = Array.from(tBody.rows);
+                        if (tRows && cols) {
+                            for (const r of tRows) {
+                                const dataRow = {};
+                                const cells = r.cells;
+                                for (const c of cells) {
+                                    const h = c.dataset.th;
+                                    dataRow[h] = c.textContent;
                                 }
+                                data.push(dataRow);
                             }
                         }
-                        return data;
-                    }, log);
-                    log.info("scraped data", tableData);
-                    await pushData(tableData);
-                }
-                else {
-                    log.error("selector not found", { selector: tableSelector });
-                }
-            },
-        });
-        // Run the crawler with the start URLs and wait for it to finish.
-        await crawler.run(startUrls);
+                        else {
+                            throw new Error("failed to scrape table header or table data");
+                        }
+                    }
+                    else {
+                        throw new Error("failed to get table body");
+                    }
+                    return data;
+                });
+                log.info("scraped data", tableData);
+                await pushData(tableData);
+            }
+            catch (error) {
+                log.error(error.message);
+            }
+        },
     });
-})();
+    // Run the crawler with the start URLs and wait for it to finish.
+    await crawler.run(startUrls);
+});
 //# sourceMappingURL=main.js.map
